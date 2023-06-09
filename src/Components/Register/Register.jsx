95e1f1426e270f42ac8a52a2ebf1f9d0838ac0cd
MohammadHalaweh7 <s11940976@stu.najah.edu>
2023-06-09 21:36:58 +0300

    Validation of the Register page

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,23 +1,35 @@
 import axios from "axios";
 import { useFormik } from "formik";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as Yup from 'yup';
 
 export default function Register() {
   let [errors, setErrors] = useState([]);
   let [statusError, setStatusError] = useState("");
+  const navigate = useNavigate();
+
+  const schema = Yup.object({
+    userName:Yup.string().required("name is required").min(3,"min is 3 char").max(10,"max is 10 char"),
+    email:Yup.string().required("email is required").email("not valid email"),
+    password:Yup.string().required("password is required").matches(/^[A-Z][a-z0-9]{3,7}$/),
+    cPassword:Yup.string().required("Confirm password is required").oneOf([Yup.ref('password')],'not match password')
+  })
   let formik = useFormik({
     initialValues: {
-      name: "",
+      userName: "",
       email: "",
       password: "",
       cPassword: "",
-    },
+    },validationSchema:schema,
     onSubmit: sendRegisterData,
   });
+
+   
   async function sendRegisterData(values) {
     let { data } = await axios
       .post(
-        "https://lazy-blue-sockeye-gear.cyclic.app/api/v1/auth/signup",
+        "https://precious-bass-tights.cyclic.app/auth/signup'",
         values
       )
       .catch((err) => {
@@ -25,7 +37,11 @@ export default function Register() {
       });
     console.log(data);
     if (data.message === "success") {
+      setErrors([])
+      setStatusError('')
+      navigate('/login');
       console.log("welcome");
+      
     } else {
       setErrors(data.err[0]);
     }
@@ -38,14 +54,16 @@ export default function Register() {
         return <div className="text-danger bg-light">{error.message}</div>;
       })}
       <form action="" onSubmit={formik.handleSubmit}>
-        <label htmlFor="name">Name</label>
+        <label htmlFor="userName">Name</label>
         <input
           type="text"
-          name="name"
+          name="userName"
           className="form-control my-3"
-          value={formik.values.name}
+          value={formik.values.userName}
           onChange={formik.handleChange}
         />
+
+        {formik.errors.userName?<p className="alert alert-danger">{formik.errors.userName}</p>:''}
 
         <label htmlFor="name">Email</label>
         <input
@@ -55,6 +73,7 @@ export default function Register() {
           value={formik.values.email}
           onChange={formik.handleChange}
         />
+        {formik.errors.email?<p className="alert alert-danger">{formik.errors.email}</p>:''}
 
         <label htmlFor="name">Password</label>
         <input
@@ -64,6 +83,9 @@ export default function Register() {
           value={formik.values.password}
           onChange={formik.handleChange}
         />
+        {formik.errors.password?<p className="alert alert-danger">{formik.errors.password}</p>:''}
+
+        
 
         <label htmlFor="name">Confirm Password</label>
         <input
@@ -73,6 +95,7 @@ export default function Register() {
           value={formik.values.cPassword}
           onChange={formik.handleChange}
         />
+        {formik.errors.cPassword?<p className="alert alert-danger">{formik.errors.cPassword}</p>:''}
 
         <button type="submit" className="btn btn-info mt-3">
           Register
