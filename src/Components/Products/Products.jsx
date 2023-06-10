44a139326fd085eabaa1336f9dce20acaece6127
MohammadHalaweh7 <s11940976@stu.najah.edu>
2023-06-11 00:27:50 +0300

    Add product page

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export default function Products() {
+
+  const[products,setProducts]=useState([]);
+
+  async function getProducts(){
+    const {data} =await axios.get('https://king-prawn-app-3mgea.ondigitalocean.app/product');
+    setProducts(data.products)
+  }
+
+  useEffect(()=>{
+    getProducts();
+  },[])
+
   return (
-    <div>Products</div>
+    <div className='row'>
+      <div className="col-md-4">
+        {
+          products.map((product)=>
+          <div>
+            <img src={product.mainImage.secure_url} alt={product.name} />
+            <p>{product.name}</p>
+          </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
